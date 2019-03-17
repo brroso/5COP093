9b6d24551d48b106444cdd3c548cd692809c173e
brroso <joaov98@gmail.com>
2019-03-17 19:46:21 -0300

    fixed

--- main.py
+++ main.py
@@ -40,6 +40,9 @@ def main(argv):
     print('Input file is "', input_file, '"')
     print('Output file is "', output_file, '"')
     output = open(output_file, "w")
+    with open(input_file, "a") as appending:
+        appending.write('\n')
+        appending.close()
     with open(input_file, "r") as f:
         while True:
             c = f.read(1)
