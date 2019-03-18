6679bb97f36dacad61ccdcdbb93df30ce8b0499c
Brroso <joaov98@gmail.com>
2019-03-18 16:04:24 -0300

    finished get_column

--- main.py
+++ main.py
@@ -84,6 +84,44 @@ numbers = [
             ]
 
 def get_column(c):
+    if c == ':':
+        return 1
+    if c == '(':
+        return 2
+    if c == '*':
+        return 3
+    if c == '.':
+        return 4
+    if c == '>':
+        return 5
+    if c == '<':
+        return 6
+    if c == '\':
+        return 7
+    if c == ',':
+        return 8
+    if c == ';':
+        return 9
+    if c == ')':
+        return 10
+    if c == '=':
+        return 11
+    if c == '*':
+        return 12
+    if c == '[':
+        return 13
+    if c == ']':
+        return 14
+    if c == '{':
+        return 15
+    if c == '}':
+        return 16
+    if c == '_':
+        return 17
+    if c == '-':
+        return 18
+    if c == '+':
+        return 19
     if c in letters or c in cap_letters:
         return 20
     if c in numbers:
