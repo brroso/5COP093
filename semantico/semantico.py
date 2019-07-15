921fbce63702f61e577064eb89198a80dfa6d39f
brroso <joaov98@gmail.com>
2019-07-15 14:30:37 -0300

    start

--- semantico/semantico.py
+++ semantico/semantico.py
@@ -3,7 +3,31 @@ from modules import *
 
 ast = open('../ast', 'rb')
 ast = pickle.load(ast)
-for pre, fill, node in ast:
-    print(pre, node.name)
-    if node.name == 'tabela de simbolos':
-        print_hash(node.ht)
+
+
+
+
+def main(argv):
+    global ast
+    for pre, fill, node in ast:
+        print(pre, node.name)
+        if node.name == 'var declaration':
+            # vardec
+        elif node.name == 'atribuicao':
+            # atrib
+        elif node.name == 'procedure dec':
+            # procdec
+        elif 'Proc call' in node.name:
+            # proccall
+        elif node.name == 'function dec':
+            # funcdec
+        elif 'Function call' in node.name:
+            # funccall
+        elif node.name == 'Write':
+            # write
+        elif node.name == 'Read':
+            # read
+        elif node.name in operations:
+            # comparacao
+        
+
