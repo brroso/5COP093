50da364a5b16355c9b6699502be8d6b03a07feaa
brroso <joaov98@gmail.com>
2019-07-26 14:52:42 -0300

    passagem n desloc

--- semantico/semantico.py
+++ semantico/semantico.py
@@ -28,10 +28,12 @@ def semantigo(node, first):  # Inicia a análise
 
 
 class variavel(object):
-    def __init__(self, name, tipo, nivel):
+    def __init__(self, name, tipo, nivel, passagem=None, desloc=None):
         self.name = name
         self.tipo = tipo
         self.nivel = nivel
+        self.passagem = passagem
+        self.desloc = desloc
 
 
 class routine(object):
@@ -49,29 +51,43 @@ def routine_dec(node):  # lida com declarações de rotinas
 
     var_list = []
     name = node.children[0].name  # o nome da rotina=sempre seu primeiro filho
-    print('rotina', name)
-    for filho in node.children:
+    for no in node.children:  # procura a tabela de símbolos para salvar no nó
+        if no.name == 'tabela de simbolos':
+            symTab = no.ht
+    for filho in node.children:  # coloca os parametros formais como variáveis
         if filho.name == name:
             continue
         elif filho.name == 'bloco':
             break
         else:
-            var = variavel(filho.name, filho.children[0].name, nivel)
-            var_list.append(var)
-    for no in node.children:  # procura a tabela de símbolos para salvar no nó
-        if no.name == 'tabela de simbolos':
-            symTab = no.ht
+            for lista in symTab:
+                for item in lista:
+                    # Checa se existe rotina com o nome do forpar
+                    for rotina in routines_battery:
+                        if rotina.name == filho.name:
+                            print("erro: Parâmetro formal", filho.name,
+                                  "da rotina", name,
+                                  "não pode ser declarado pois existe" +
+                                  " rotina com esse nome.")
+                            quit()
+                    # Pega o deslocamento e tipo de passagem do forpar
+                    if item.getCategory() == 'parametro formal' and \
+                            item.getName() == filho.name:
+                        var = variavel(filho.name, filho.children[0].name,
+                                       nivel, item.getPassagem(),
+                                       item.getDesloc())
+                        var_list.append(var)
     rout = routine(name, symTab, current_routine)  # cria a rotina em si
     for rotina in routines_battery:
         if rotina.name == rout.name:  # checa se já tem uma rotina c esse nome
-            print('erro semântico: já há um identificador visivel de nome',
+            print('erro: já há um identificador visivel de nome',
                   rout.name)
             quit()
     else:  # checa se já tem uma variavel com esse nome no <= nivel
         for lista in var_battery:
             for identificador in lista:
                 if identificador.name == rout.name:
-                    print('erro semântico: já há um identificador visivel',
+                    print('erro: já há um identificador visivel',
                           'de nome', rout.name)
                     quit()
     nivel += 1
@@ -94,13 +110,13 @@ def var_dec(node):
         for variable in var_battery[-1]:
             # Caso já tenha na lista atual uma variavel com esse nome:
             if variable.name == var.name:
-                print('Já existe variável visível de nome', var.name)
+                print('erro: Já existe variável visível de nome', var.name)
                 quit()
         # Ve todas as rotinas da pilha
         for rotina in routines_battery:
             # Caso já tenha na pilha atual uma rotina com esse nome:
             if rotina.name == var.name:
-                print('erro semântico: já há uma rotina visivel',
+                print('erro: já há uma rotina visivel',
                       'de nome', var.name)
                 quit()
         var_battery[-1].append(var)
@@ -131,7 +147,7 @@ def main(argv):
         # caso encontre uma dec de proc na arvre:
         elif node.name == 'procedure dec':
             routine_dec(node)
-            
+
         # elif 'Proc call' in node.name:
         #     # proccall
 
